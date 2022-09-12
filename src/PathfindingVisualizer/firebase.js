e7d5a7d6abaf790ef050b52cfc6447441d331e34
Manojna007 <1rn19cs076.manojnap@gmailcom>
2022-09-12 19:26:35 +0530

    first commit

---
+++ src/PathfindingVisualizer/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1v0fR2NlKg5u4WSEBKvqSuYjIzD7qGPA",
+    authDomain: "visualize-dijkstra-s-algo.firebaseapp.com",
+    databaseURL: "https://visualize-dijkstra-s-algo-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "visualize-dijkstra-s-algo",
+    storageBucket: "visualize-dijkstra-s-algo.appspot.com",
+    messagingSenderId: "938592158211",
+    appId: "1:938592158211:web:a350b945ccc2ca977cc436"
+  };
